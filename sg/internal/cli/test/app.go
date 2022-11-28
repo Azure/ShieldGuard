bd4f1c2fa3c70219f27cf2a1719ec309851e1916
hbc <bahe@microsoft.com>
2022-11-28 14:19:02 -0800

    feat: present output

--- sg/internal/cli/test/app.go
+++ sg/internal/cli/test/app.go
@@ -3,9 +3,12 @@ package test
 import (
 	"context"
 	"fmt"
+	"io"
 	"path/filepath"
 
 	"github.com/Azure/ShieldGuard/sg/internal/project"
+	"github.com/Azure/ShieldGuard/sg/internal/result"
+	"github.com/Azure/ShieldGuard/sg/internal/result/presenter"
 	"github.com/spf13/pflag"
 )
 
@@ -13,10 +16,15 @@ import (
 type cliApp struct {
 	projectSpecFile string
 	contextRoot     string
+	outputFormat    string
+
+	stdout io.Writer
 }
 
 func newCliApp() *cliApp {
-	return &cliApp{}
+	return &cliApp{
+		outputFormat: presenter.FormatJSON,
+	}
 }
 
 func (cliApp *cliApp) Run() error {
@@ -32,10 +40,18 @@ func (cliApp *cliApp) Run() error {
 		return fmt.Errorf("read project spec: %w", err)
 	}
 
+	var queryResultsList []result.QueryResults
 	for _, target := range projectSpec.Files {
-		if err := cliApp.runFileTarget(ctx, cliApp.contextRoot, target); err != nil {
+		queryResult, err := cliApp.queryFileTarget(ctx, cliApp.contextRoot, target)
+		if err != nil {
 			return fmt.Errorf("run target (%s): %w", target.Name, err)
 		}
+		queryResultsList = append(queryResultsList, *queryResult)
+	}
+
+	if err := presenter.QueryResultsList(cliApp.outputFormat, queryResultsList).
+		WriteQueryResultTo(cliApp.stdout); err != nil {
+		return fmt.Errorf("write query results: %w", err)
 	}
 
 	return nil
@@ -43,6 +59,10 @@ func (cliApp *cliApp) Run() error {
 
 func (cliApp *cliApp) BindCLIFlags(fs *pflag.FlagSet) {
 	fs.StringVarP(&cliApp.projectSpecFile, "config", "c", project.SpecFileName, "Path to the project spec file.")
+	fs.StringVarP(
+		&cliApp.outputFormat, "output", "o", cliApp.outputFormat,
+		fmt.Sprintf("Output format. Available formats: %s", presenter.AvailableFormatsHelp()),
+	)
 }
 
 func (cliApp *cliApp) defaults() error {
@@ -64,13 +84,20 @@ func (cliApp *cliApp) defaults() error {
 		return fmt.Errorf("failed to get absolute path of the context root: %w", err)
 	}
 
+	if _, exists := presenter.AvailableFormats[cliApp.outputFormat]; !exists {
+		return fmt.Errorf(
+			"output format %q is not supported. Supported formats are: %s",
+			cliApp.outputFormat, presenter.AvailableFormatsHelp(),
+		)
+	}
+
 	return nil
 }
 
-func (cliApp *cliApp) runFileTarget(
+func (cliApp *cliApp) queryFileTarget(
 	ctx context.Context,
 	contextRoot string,
 	target project.FileTargetSpec,
-) error {
-	return nil
+) (*result.QueryResults, error) {
+	return nil, nil
 }
