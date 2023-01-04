12a8db3ad57545426576d7f49a7536f139410402
hbc <bahe@microsoft.com>
2023-01-04 10:54:38 -0800

    fix: aligning struct tags

--- sg/internal/project/types.go
+++ sg/internal/project/types.go
@@ -2,18 +2,18 @@ package project
 
 // Spec defines the project specification.
 type Spec struct {
-	Files []FileTargetSpec `json:"files"`
+	Files []FileTargetSpec `json:"files" yaml:"files"`
 }
 
 // FileTargetSpec defines the specification of a file target.
 // Without further specification, paths are relative to the context root which is defined during execution.
 type FileTargetSpec struct {
 	// Name - name of the target.
-	Name string `yaml:"name"`
+	Name string `json:"name" yaml:"name"`
 	// Paths - paths to the targets to check.
-	Paths []string `yaml:"paths"`
+	Paths []string `json:"paths" yaml:"paths"`
 	// Policies - paths to the policy to load.
-	Policies []string `yaml:"policies"`
+	Policies []string `json:"policies" yaml:"policies"`
 	// Data - paths to the (extra) data to load.
-	Data []string `yaml:"data"`
+	Data []string `json:"data" yaml:"data"`
 }
