4b73153a7998cb53c684ec8f1006aae8e22a8895
Jing Gao <jing@JingMS>
2023-01-28 00:02:12 -0800

    Enable starting a new line for each result for text presenter

--- sg/internal/result/presenter/text.go
+++ sg/internal/result/presenter/text.go
@@ -31,17 +31,17 @@ func Text(queryResultsList []result.QueryResults) WriteQueryResultTo {
 			totalTest += totalPass + totalFailures + totalWarnings + totalExceptions
 
 			for _, failureResultObj := range queryResultObj.Failures {
-				fmt.Fprintf(w, "FAIL - %s - %s - %s", queryResultObj.Filename, queryResultObj.Namespace, failureResultObj.Message)
+				fmt.Fprintf(w, "FAIL - %s - %s - %s\n", queryResultObj.Filename, queryResultObj.Namespace, failureResultObj.Message)
 			}
 			for _, warningResultObj := range queryResultObj.Warnings {
-				fmt.Fprintf(w, "WARN - %s - %s - %s", queryResultObj.Filename, queryResultObj.Namespace, warningResultObj.Message)
+				fmt.Fprintf(w, "WARN - %s - %s - %s\n", queryResultObj.Filename, queryResultObj.Namespace, warningResultObj.Message)
 			}
 			for _, exceptionResultObj := range queryResultObj.Exceptions {
-				fmt.Fprintf(w, "Exception - %s - %s - %s", queryResultObj.Filename, queryResultObj.Namespace, exceptionResultObj.Message)
+				fmt.Fprintf(w, "Exception - %s - %s - %s\n", queryResultObj.Filename, queryResultObj.Namespace, exceptionResultObj.Message)
 			}
 		}
 
-		fmt.Fprintf(w, "%d tests, %d passed, %d failures %d warnings, %d exceptions", totalTest, totalPass, totalFailures, totalWarnings, totalExceptions)
+		fmt.Fprintf(w, "%d tests, %d passed, %d failures %d warnings, %d exceptions\n", totalTest, totalPass, totalFailures, totalWarnings, totalExceptions)
 
 		return nil
 	})
