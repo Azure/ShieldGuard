52a51183325a41c6741c23a336f183b112fb5c8a
Jing Gao <jing@JingMS>
2023-01-31 21:15:10 -0800

    Fix logic of calculation on var totalTests in func Text()

--- sg/internal/result/presenter/text.go
+++ sg/internal/result/presenter/text.go
@@ -13,22 +13,20 @@ func Text(queryResultsList []result.QueryResults) WriteQueryResultTo {
 	queryResultsObjList := asQueryResultsObjList(queryResultsList)
 
 	return writeQueryResultToFunc(func(w io.Writer) error {
-		var totalTest int
-		var totalPass int
+		var totalTests int
+		var totalPasses int
 		var totalFailures int
 		var totalWarnings int
 		var totalExceptions int
 
 		for _, queryResultObj := range queryResultsObjList { // TODO: maybe we should sort & group results before iterating them
-			totalPass += queryResultObj.Success
+			totalPasses += queryResultObj.Success
 
 			totalFailures += len(queryResultObj.Failures)
 
 			totalWarnings += len(queryResultObj.Warnings)
 
 			totalExceptions += len(queryResultObj.Exceptions)
-
-			totalTest += totalPass + totalFailures + totalWarnings + totalExceptions
 
 			for _, failureResultObj := range queryResultObj.Failures {
 				fmt.Fprintf(w, "FAIL - %s - %s - %s\n", queryResultObj.Filename, queryResultObj.Namespace, failureResultObj.Message)
@@ -37,16 +35,13 @@ func Text(queryResultsList []result.QueryResults) WriteQueryResultTo {
 				fmt.Fprintf(w, "WARN - %s - %s - %s\n", queryResultObj.Filename, queryResultObj.Namespace, warningResultObj.Message)
 			}
 			for _, exceptionResultObj := range queryResultObj.Exceptions {
-				fmt.Fprintf(w, "Exception - %s - %s - %s\n", queryResultObj.Filename, queryResultObj.Namespace, exceptionResultObj.Message)
+				fmt.Fprintf(w, "EXCEPTION - %s - %s - %s\n", queryResultObj.Filename, queryResultObj.Namespace, exceptionResultObj.Message)
 			}
 		}
 
-		fmt.Fprintf(w, "%d tests, %d passed, %d failures %d warnings, %d exceptions\n", totalTest, totalPass, totalFailures, totalWarnings, totalExceptions)
+		totalTests = totalPasses + totalFailures + totalWarnings + totalExceptions
+		fmt.Fprintf(w, "%d tests, %d passed, %d failures %d warnings, %d exceptions\n", totalTests, totalPasses, totalFailures, totalWarnings, totalExceptions)
 
 		return nil
 	})
-}
-
-func String(totalTest int) {
-	panic("unimplemented")
 }
