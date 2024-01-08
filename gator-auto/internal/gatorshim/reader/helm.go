57d7825afdf858dad134855152bf496bc1cf8164
hbc <bahe@microsoft.com>
2024-01-08 13:44:08 -0800

    feat: split by source file

--- gator-auto/internal/gatorshim/reader/helm.go
+++ gator-auto/internal/gatorshim/reader/helm.go
@@ -1,14 +1,81 @@
 package reader
 
 import (
+	"bufio"
 	"bytes"
 	"context"
+	"io"
 	"os/exec"
+	"strings"
 	"time"
 
 	gatorreader "github.com/open-policy-agent/gatekeeper/v3/pkg/gator/reader"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
+
+// helmSourceDelimitedReaders delimits the helm rendered chart output using the `# Source: ...` annotation.
+func helmSourceDelimitedReaders(source io.Reader) (map[string]io.Reader, error) {
+	const unknownFile = ""
+	const sourceAnnotationPrefix = "# Source: "
+
+	fileBuffers := make(map[string]*bytes.Buffer)
+	var (
+		currentFile       string
+		currentFileBuffer *bytes.Buffer
+	)
+
+	saveCurrentFileBuffer := func() error {
+		if currentFileBuffer == nil {
+			return nil
+		}
+
+		previousBuffer, ok := fileBuffers[currentFile]
+		if ok {
+			if _, err := previousBuffer.Write(currentFileBuffer.Bytes()); err != nil {
+				return err
+			}
+		} else {
+			fileBuffers[currentFile] = currentFileBuffer
+		}
+
+		currentFileBuffer = nil
+		currentFile = unknownFile
+
+		return nil
+	}
+
+	s := bufio.NewScanner(source)
+	for s.Scan() {
+		line := s.Text()
+
+		// start a new file buffer
+		if strings.HasPrefix(line, sourceAnnotationPrefix) {
+			if err := saveCurrentFileBuffer(); err != nil {
+				return nil, err
+			}
+
+			currentFile = strings.TrimPrefix(line, sourceAnnotationPrefix)
+		}
+
+		if currentFileBuffer == nil {
+			currentFileBuffer = &bytes.Buffer{}
+		}
+		if _, err := currentFileBuffer.WriteString(line + "\n"); err != nil {
+			return nil, err
+		}
+	}
+	// save the last file buffer
+	if err := saveCurrentFileBuffer(); err != nil {
+		return nil, err
+	}
+
+	rv := make(map[string]io.Reader)
+	for file, buffer := range fileBuffers {
+		rv[file] = buffer
+	}
+
+	return rv, nil
+}
 
 func resolveHelmCommand(helmCommand string) (string, error) {
 	if helmCommand == "" {
@@ -41,7 +108,7 @@ func readHelmChart(
 		return nil, err
 	}
 
-	us, err := gatorreader.ReadK8sResources(stdout)
+	readers, err := helmSourceDelimitedReaders(stdout)
 	if err != nil {
 		return nil, err
 	}
@@ -50,12 +117,19 @@ func readHelmChart(
 		ObjectSources: make(map[*unstructured.Unstructured]ObjectSource),
 	}
 
-	rv.Objects = append(rv.Objects, us...)
-	for _, obj := range us {
-		rv.ObjectSources[obj] = ObjectSource{
-			SourceType: SourceTypeHelm,
-			// TODO: read from `# Source: ...` annotation?
-			FilePath: chart,
+	for filePath, reader := range readers {
+		us, err := gatorreader.ReadK8sResources(reader)
+		if err != nil {
+			return nil, err
+		}
+
+		rv.Objects = append(rv.Objects, us...)
+		for _, obj := range us {
+			rv.ObjectSources[obj] = ObjectSource{
+				SourceType: SourceTypeHelm,
+				FilePath:   filePath,
+				// TODO: save chart reference
+			}
 		}
 	}
 
