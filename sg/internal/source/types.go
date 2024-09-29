8fe55ffd96a79b7229f60ed6fd9dc2d505345349
hbc <bahe@microsoft.com>
2024-09-29 18:48:51 +0000

    perf: parse configuration once

--- sg/internal/source/types.go
+++ sg/internal/source/types.go
@@ -1,9 +1,11 @@
 package source
 
+import "github.com/open-policy-agent/opa/ast"
+
 // Source is the interface for a validation source.
 type Source interface {
 	// Name returns the name of the target.
 	Name() string
 	// ParsedConfigurations returns the parsed configurations of the target.
-	ParsedConfigurations() ([]interface{}, error)
+	ParsedConfigurations() ([]ast.Value, error)
 }
