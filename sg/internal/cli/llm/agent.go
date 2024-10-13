fcc26c29d3dea7d83721191538e5ca2b70e937dd
hbc <bahe@microsoft.com>
2024-10-13 22:39:48 +0000

    feat: poc

--- sg/internal/cli/llm/agent.go
+++ sg/internal/cli/llm/agent.go
@@ -1,6 +1,9 @@
 package llm
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/Azure/ShieldGuard/sg/internal/llm/swarm"
 	"github.com/openai/openai-go"
 )
@@ -27,15 +30,46 @@ var AgentAnalyzeServiceAccountUsage = swarm.AgentDecl{
 
 var analyzeServiceAccountUsage = swarm.AgentToFunction(AgentAnalyzeServiceAccountUsage)
 
+var analyzeServicePort = swarm.ToAgentFunctionOrPanic(
+	"check-port",
+	"Analyze the exposed port for a service. The first argument is the k8s service type (LoadBalancer / ClusterIP / NodePort etc) and the second argument is the port number. Invoke more times to check multiple ports.",
+	func(svcType string, port int64) (string, error) {
+		if strings.ToLower(svcType) == "clusterip" {
+			return "Service type is ClusterIP. No need to check exposed ports.", nil
+		}
+
+		if port == 80 {
+			return "Port 80 is exposed. Consider using HTTPS instead.", nil
+		}
+		if port == 443 {
+			return "Port 443 is exposed. Make sure it is secure.", nil
+		}
+		return fmt.Sprintf("Port %d is exposed. Check if it is necessary.", port), nil
+	},
+)
+
+var AgentAnalyzeServicePort = swarm.AgentDecl{
+	Name:              "analyze-service-ports",
+	Description:       `Analyze the exposed ports for a service.`,
+	Model:             openai.ChatModelGPT4o,
+	Instructions:      swarm.AgentInstructions(`Analyze the exposed ports from the declared spec. Inspect evert service and port combination. Report any potential security risks.`),
+	ToolChoice:        openai.ChatCompletionToolChoiceOptionStringAuto,
+	ParallelToolsCall: true,
+	Functions: []swarm.AgentFunction{
+		analyzeServicePort,
+	},
+}.Build()
+
 var AgentTriage = swarm.AgentDecl{
 	Name:         "triage",
 	Description:  `Triage the input text.`,
 	Model:        openai.ChatModelGPT4o,
-	Instructions: swarm.AgentInstructions("Read and understand the input text. React based on the content."),
+	Instructions: swarm.AgentInstructions("Read and understand the input text. React based on the suer input content. You can perform multiple analysis in parallel. Summarize your findings."),
 	ToolChoice:   openai.ChatCompletionToolChoiceOptionStringAuto,
 	Functions: []swarm.AgentFunction{
 		summary,
 		analyzeServiceAccountUsage,
+		swarm.AgentToFunction(AgentAnalyzeServicePort),
 	},
 	ParallelToolsCall: true,
 }.Build()
