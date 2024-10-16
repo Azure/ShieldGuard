bed63e5b85ff10fe16d7a5ea09a3cdbe4a830dac
Ubuntu <stathomas@devbox.m4sem0u2cxoublkapxi1el1s1c.xx.internal.cloudapp.net>
2024-10-16 14:24:48 +0000

    test: updated builder tests

--- sg/internal/source/builder_test.go
+++ sg/internal/source/builder_test.go
@@ -9,30 +9,32 @@ import (
 func Test_SourceBuilder_FromPath(t *testing.T) {
 	sources, err := FromPath([]string{"./testdata/sample"}).Complete()
 	assert.NoError(t, err)
-	assert.Len(t, sources, 2)
+	assert.Len(t, sources, 3)
 }
 
 func Test_SourceBuilder_ContextRoot(t *testing.T) {
 	t.Run("empty context root", func(t *testing.T) {
 		sources, err := FromPath([]string{"./testdata/sample"}).ContextRoot("").Complete()
 		assert.NoError(t, err)
-		assert.Len(t, sources, 2)
+		assert.Len(t, sources, 3)
 	})
 
 	t.Run("relative context root", func(t *testing.T) {
 		sources, err := FromPath([]string{"./testdata/sample"}).ContextRoot("./testdata").Complete()
 		assert.NoError(t, err)
-		assert.Len(t, sources, 2)
+		assert.Len(t, sources, 3)
 		assert.Equal(t, "sample/deployment+service.yaml", sources[0].Name())
 		assert.Equal(t, "sample/service.yaml", sources[1].Name())
+		assert.Equal(t, "sample/template.json", sources[2].Name())
 	})
 
 	t.Run("relative context root nested", func(t *testing.T) {
 		sources, err := FromPath([]string{"./testdata/sample"}).ContextRoot("./testdata/sample").Complete()
 		assert.NoError(t, err)
-		assert.Len(t, sources, 2)
+		assert.Len(t, sources, 3)
 		assert.Equal(t, "deployment+service.yaml", sources[0].Name())
 		assert.Equal(t, "service.yaml", sources[1].Name())
+		assert.Equal(t, "template.json", sources[2].Name())
 	})
 
 	t.Run("non-relative context root", func(t *testing.T) {
